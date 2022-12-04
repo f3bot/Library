c9e6537b7b7e67f5465cf2969374bce868d003f0
f3bot <110943732+f3bot@users.noreply.github.com>
2022-12-03 18:54:48 -0800

    Add files via upload
    update

--- main.js
+++ main.js
@@ -2,66 +2,72 @@ const titleint = document.getElementById('title')
 const authorint = document.getElementById('author')
 const pagesint = document.getElementById('pages')
 const submit = document.getElementById('submit')
-const bookcontainer = document.getElementsByClassName('book-container')
-let checkbox = document.querySelector('.checkbox')
+const bookcontainer = document.querySelector('.book-container')
+checkbox = document.querySelector('.checkbox')
 let myLibrary = []
+let counter = 0
 
-const book = {
-    title: '',
-    author: '',
-    pages: ''
+function Book(title,author,pages,status)
+{
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
 }
 
-let uniqueLibrary = []
-
-const bookToArray = () =>{
+const addBook = () =>{
+    const book = new Book
     book.title = titleint.value
     book.author = authorint.value
     book.pages = pagesint.value
-    myLibrary.push(book.title, book.author, book.pages)
-    resetInput()
-    uniqueLibrary = [...new Set(myLibrary)];
-    console.log(uniqueLibrary)
-    console.log(uniqueLibrary.length)
-    for(let i = 0; i < uniqueLibrary.length; i++){
-        let div = document.createElement('div')
-        let titlespan = document.createElement('span')
-        titlespan.innerHTML = uniqueLibrary[i]
-        div.appendChild(titlespan)
-        let authorspan = document.createElement('span')
-        authorspan.innerHTML = uniqueLibrary[i+1]
-        div.appendChild(authorspan)
-        let pagesspan = document.createElement('span')
-        pagesspan.innerHTML = uniqueLibrary[i+2]
-        div.appendChild(pagesspan)
-        let img = document.createElement('img')
-        if(checkbox.checked == true){
-            console.log("true")
-            img.src = "yes.png"
-        }else{
-            console.log("false")
-            img.src = "no.png"
-        }
-        div.appendChild(img)
-        let deletebutton = document.createElement('button')
-        deletebutton.innerHTML = 'Delete Book'
-        deletebutton.addEventListener('click', () =>{
-            div.remove()
-        })
-        div.appendChild(deletebutton)
-        console.log(div)
+    if(checkbox.value == true){
+        book.status = 'Read'
+    }else{
+        book.status = "Not Read"
     }
-    bookcontainer.appendChild(div)
+    if(book.title.length === 0 || book.author.length === 0 || book.pages.length === 0){
+        console.log("null")
+    }else{
+        if(counter % 5 == 0){
+            const row = document.createElement('div')
+            row.classList.add('div-row')
+            bookcontainer.appendChild(row)
+        }else{
+            
+        }
+    }
 }
 
 const resetInput = () =>{
     titleint.value = ''
-    authorint.value = ''
     pagesint.value = ''
+    authorint.value = ''
 }
-
 
 submit.addEventListener('click', (e) =>{
     e.preventDefault()
-    bookToArray()
+    addBook()
+    resetInput()
+    counter++
+    console.log(counter)
 })
+
+/*
+        myLibrary.push(book)
+        let div = document.createElement('div')
+        div.classList.add('book-div')
+        let tspan = document.createElement('span')
+        tspan.textContent = book.title;
+        div.appendChild(tspan)
+        let aspan = document.createElement('span')
+        aspan.textContent = book.author
+        div.appendChild(aspan)
+        let pspan = document.createElement('span')
+        pspan.textContent = book.pages
+        div.appendChild(pspan)
+        let sspan = document.createElement('span')
+        sspan.textContent = book.status
+        div.appendChild(sspan)
+        bookcontainer.appendChild(div)
+        console.table(myLibrary)
+*/
