47e0eb6166fe199a0d9bc9aa14f42e712ee69216
f3bot <febot@protonmail.com>
2022-12-04 20:14:07 +0100

    update
    #added footer
    #css updates
    #started working on pop-up styling

--- main.js
+++ main.js
@@ -3,9 +3,20 @@ const authorint = document.getElementById('author')
 const pagesint = document.getElementById('pages')
 const submit = document.getElementById('submit')
 const bookcontainer = document.querySelector('.book-container')
-checkbox = document.querySelector('.checkbox')
+const loginPopUp = document.querySelector('.popup')
+let checkbox = document.querySelector('.checkbox')
 let myLibrary = []
 let counter = 0
+
+const openForm = () =>{
+    loginPopUp.style.display = 'block'
+}
+
+const closeForm = () =>{
+    loginPopUp.style.display = 'none'
+}
+
+
 
 function Book(title,author,pages,status)
 {
@@ -20,22 +31,30 @@ const addBook = () =>{
     book.title = titleint.value
     book.author = authorint.value
     book.pages = pagesint.value
-    if(checkbox.value == true){
-        book.status = 'Read'
-    }else{
-        book.status = "Not Read"
-    }
-    if(book.title.length === 0 || book.author.length === 0 || book.pages.length === 0){
-        console.log("null")
-    }else{
-        if(counter % 5 == 0){
-            const row = document.createElement('div')
-            row.classList.add('div-row')
-            bookcontainer.appendChild(row)
-        }else{
-            
-        }
-    }
+    myLibrary.push(book)
+    let div = document.createElement('div')
+    div.classList.add('book-div')
+    let tspan = document.createElement('span')
+    tspan.classList.add('Booktitle')
+    tspan.textContent = book.title;
+    div.appendChild(tspan)
+    let aspan = document.createElement('span')
+    aspan.textContent = book.author
+    div.appendChild(aspan)
+    let pspan = document.createElement('span')
+    pspan.textContent = book.pages + " Pages"
+    div.appendChild(pspan)
+    let sspan = document.createElement('button')
+    sspan.textContent = 'Not Read'
+    sspan.style.background = 'red'
+    div.appendChild(sspan)
+    let rbutton = document.createElement('button')
+    rbutton.textContent = 'Remove'
+    div.appendChild(rbutton)
+    bookcontainer.appendChild(div)
+    console.table(myLibrary)
+    readEvent(sspan)
+    removeEvent(rbutton,div)
 }
 
 const resetInput = () =>{
@@ -48,9 +67,25 @@ submit.addEventListener('click', (e) =>{
     e.preventDefault()
     addBook()
     resetInput()
-    counter++
-    console.log(counter)
 })
+
+const readEvent = (element) =>{
+    element.addEventListener('click', () =>{
+        if(element.textContent === 'Read'){
+            element.textContent = 'Not Read'
+            element.style.background = 'red'
+        }else if(element.textContent === 'Not Read'){
+            element.textContent = 'Read'
+            element.style.background = 'green'
+        }
+    })
+}
+
+const removeEvent = (e,target) =>{
+    e.addEventListener('click', () =>{
+        target.remove();
+    })
+}
 
 /*
         myLibrary.push(book)
